7b60a177d536fbaf6786b6d8cb6943743cd0a7ad
Pedro Pablo Vilchez Carrasquero <vilchezpedro95@gmail.com>
2017-04-05 10:47:24 -0400

    Añadido Enemigo y su AI mediante navmesh

---
+++ Dungeon Arena Rushers/Assets/Scripts/Enemies/EnemyMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour {
+
+	Transform Player;
+	NavMeshAgent navA;
+	// Use this for initialization
+	void Awake () {
+
+		Player = GameObject.FindGameObjectWithTag ("Player").transform;
+		navA = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		navA.SetDestination (Player.position);
+		
+	}
+}
